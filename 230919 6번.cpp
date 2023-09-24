57be3a37ca4e473883559eb0726eee345ac86ebf
송호근 <115344824+hogeunSong@users.noreply.github.com>
2023-09-24 17:40:22 +0900

    Add files via upload

---
+++ 230919 6번.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+void print_D0B(int year = 1990, int month = 1, int day = 1) {
+
+	cout << "생년월일은 " << year << "년 " << month << "월 " << day << "일입니다." << endl;
+}
+
+int main() {
+	print_D0B();
+
+	int year, month, day;
+	cout << "year 입력: ";
+	cin >> year;
+	cout << "month 입력: ";
+	cin >> month;
+	cout << "day 입력: ";
+	cin >> day;
+
+	print_D0B(year, month, day);
+}
+
+
+
+
