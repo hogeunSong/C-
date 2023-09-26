06cc4ab1db8efd872c6681b145409ac30d6a6afe
송호근 <115344824+hogeunSong@users.noreply.github.com>
2023-09-26 21:31:53 +0900

    Add files via upload

---
+++ 230926 1번.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	char grade = 'X';
+	int score;
+	cout << "Enter your score: ";
+	cin >> score;
+
+	if (0 <= score && score <= 100) {
+		if (score < 60) {
+			char grade = 'F';
+			cout << "Your grade is " << grade << endl;
+		}
+		else if (score < 70) {
+			char grade = 'D';
+			cout << "Your grade is " << grade << endl;
+		}
+		else if (score < 80) {
+			char grade = 'C';
+			cout << "Your grade is " << grade << endl;
+		}
+		else if (score < 90) {
+			char grade = 'B';
+			cout << "Your grade is " << grade << endl;
+		}
+		else if (score <= 100) {
+			char grade = 'A';
+			cout << "Your grade is " << grade << endl;
+		}
+
+	}
+
+	else {
+		cout << "The score(" << score << ")is invalid" << endl;
+	}
+
+	return 0;
+}
---
+++ 230926 2번.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include<iomanip>
+#include<cmath>
+using namespace std;
+
+int main() {
+	int num = 1;
+	int count = 1;
+	cout.imbue(locale(""));
+
+	while (count<=20) {
+		
+		int product;
+		product = pow(num+1, count);
+		
+		cout << count << '\t' << setw(10)<<product<<'\n';
+		count++;
+	}
+	
+	return 0;
+}
